66b714a4429f36a46ff6fd5553a718974636c93e
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-02 09:14:55 +0800

    Create Enums.lua

---
+++ NebulaHub/Dependencies/Manager/Enums.lua
@@ -0,0 +1,42 @@
+_G.NebulaHub.Dependencies.Manager.Enums = {
+	["UnitType"] = {
+		["Switch"] = {
+			["Value"] = 0;
+			["Name"] = "Switch";
+		};
+		["Dropdown"] = {
+			["Value"] = 1;
+			["Name"] = "Switch";
+		};
+		["Slider"] = {
+			["Value"] = 2;
+			["Name"] = "Switch";
+		};
+		["String"] = {
+			["Value"] = 3;
+			["Name"] = "Switch";
+		};
+		["Toggle"] = {
+			["Value"] = 4;
+			["Name"] = "Toggle";
+		};
+		["Info"] = {
+			["Value"] = 5;
+			["Name"] = "Info";
+		};
+	};
+	["NoticeType"] = {
+		["Notice"] = {
+			["Value"] = 0;
+			["Name"] = "Notice";
+		};
+		["Alert"] = {
+			["Value"] = 1;
+			["Name"] = "Alert";
+		};
+		["Notification"] = {
+			["Value"] = 2;
+			["Name"] = "Notification";
+		};
+	};
+};
