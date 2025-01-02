802ae9cdc28051d27302a533a03541bf7ca51a5e
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-02 09:06:30 +0800

    Create AllServices.lua

---
+++ NebulaHub/Dependencies/Manager/AllServices.lua
@@ -0,0 +1,18 @@
+return {
+	RunService = game:GetService("RunService");
+	TweenService = game:GetService("TweenService");
+	Players = game:GetService("Players");
+	UserInputService = game:GetService("UserInputService");
+	ReplicatedStorage = game:GetService("ReplicatedStorage");
+	ReplicatedFirst = game:GetService("ReplicatedFirst");
+	StarterGui = game:GetService("StarterGui");
+	Lighting = game:GetService("Lighting");
+	Workspace = game:GetService("Workspace");
+	CollectionService = game:GetService("CollectionService");
+	HttpService = game:GetService("HttpService");
+	SoundService = game:GetService("SoundService");
+	Debris = game:GetService("Debris");
+	Chat = game:GetService("Chat");
+	TextChatService = game:GetService("TextChatService");
+	Marketplace = game:GetService("MarketplaceService");	
+}
