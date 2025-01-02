68185dd0e45a45ad6370705711d0da35eea71df4
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-02 10:41:20 +0800

    Update GamesContent.lua

--- NebulaHub/GamesContent.lua
+++ NebulaHub/GamesContent.lua
@@ -1,1 +1,8 @@
-
+return {
+  [1] = {
+    GameName = "Murder Mystery 2";
+    Code = "MM2";
+    PlaceId = 142823291;
+    Loadstring = "https://raw.githubusercontent.com/yaj1skrow/Nebula-Hub/refs/heads/main/NebulaHub/Games/MurderMystery2/Main/Rundown.lua"
+  }
+}
