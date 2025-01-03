f2a48d134486ee61a6a2f3841874ca2c7d34a4a0
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-03 17:48:07 +0800

    Update Initialization.lua

--- NebulaHub/Games/Universal/Main/Initialization.lua
+++ NebulaHub/Games/Universal/Main/Initialization.lua
@@ -1,41 +1,39 @@
 return function()
-print("Loaded Universal")
-	
-  local MainAPI = getgenv().NebulaHub.API
-  	
-  local RunService = getgenv().Services.RunService;
-  local TweenService = getgenv().Services.TweenService;
-  local Players = getgenv().Services.Players;
-  	
-  local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums;
-  local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal;
-  	
-  local Management = getgenv().NebulaHub.Dependencies.Manager.Management;
+	print("Loaded Universal")
 
-getgenv().NebulaHub.Loaded["Universal"] = {}
-	
-   getgenv().NebulaHub.Loaded["Universal"].GlobalData = {}
-  
-   getgenv().NebulaHub.Loaded["Universal"].private = {}
+	local MainAPI = getgenv().NebulaHub.API
 
-  MainLogic = {
-    [1] = "https://raw.githubusercontent.com/yaj1skrow/Nebula-Hub/refs/heads/main/NebulaHub/Games/Universal/LogicAPI/Test.lua"
-  }
+	local RunService = getgenv().Services.RunService;
+	local TweenService = getgenv().Services.TweenService;
+	local Players = getgenv().Services.Players;
 
- local MainContent = Management.newContent("Universal")
-MainContent:setSideButtonTitle("Universal")
+	local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums;
+	local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal;
 
-  local function Init()
-		print(MainLogic[1])
-		 loadstring(game:HttpGet(MainLogic[1], false))()(MainContent)
+	local Management = getgenv().NebulaHub.Dependencies.Manager.Management;
+
+	getgenv().NebulaHub.Loaded["Universal"] = {}
+
+	getgenv().NebulaHub.Loaded["Universal"].GlobalData = {}
+
+	getgenv().NebulaHub.Loaded["Universal"].private = {}
+
+	MainLogic = {
+		"https://raw.githubusercontent.com/yaj1skrow/Nebula-Hub/refs/heads/main/NebulaHub/Games/Universal/LogicAPI/Test.lua"
+	}
+
+	local MainContent = Management.newContent("Universal")
+	MainContent:setSideButtonTitle("Universal")
+
+	local function Init()
 		print("Passed")
-    for val, logicApi in pairs(MainLogic) do
-     		print("trying")
- loadstring(game:HttpGet(logicApi, false))()(MainContent)
-   		print("PassedSecond")
- end
-  end
+		for val, logicApi in pairs(MainLogic) do
+			print("trying")
+			loadstring(game:HttpGet(logicApi, false))()(MainContent)
+			print("PassedSecond")
+		end
+	end
 
-  Init()
-   
+	Init()
+
 end
