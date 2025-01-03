136b74bf1e088a0962c14bce2af19aca333a2a1c
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-03 19:29:38 +0800

    Update Test.lua

--- NebulaHub/Games/Universal/LogicAPI/Test.lua
+++ NebulaHub/Games/Universal/LogicAPI/Test.lua
@@ -1,17 +1,21 @@
-print(getgenv().NebulaHub)
-print(getgenv().NebulaHub.Dependencies)
-print(getgenv().NebulaHub.Loaded["Universal"])
-print(getgenv().NebulaHub.Loaded["Universal"].GlobalData)
+-- print(getgenv().NebulaHub)
+-- print(getgenv().NebulaHub.Dependencies)
+-- print(getgenv().NebulaHub.Loaded["Universal"])
+-- print(getgenv().NebulaHub.Loaded["Universal"].GlobalData)
 
-local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal
-local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums
-local GlobalData = getgenv().NebulaHub.Loaded["Universal"].GlobalData
+-- local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal
+-- local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums
+-- local GlobalData = getgenv().NebulaHub.Loaded["Universal"].GlobalData
 
-local Player = getgenv().LocalPlayer;
-local Character = Player.Character or Player.CharacterAdded:Wait();
+-- local Player = getgenv().LocalPlayer;
+-- local Character = Player.Character or Player.CharacterAdded:Wait();
   
-local Assets = getgenv().NebulaHub.MainUI:WaitForChild("Dependencies"):WaitForChild("Assets");
-local Storage = getgenv().NebulaHub.Storage
+-- local Assets = getgenv().NebulaHub.MainUI:WaitForChild("Dependencies"):WaitForChild("Assets");
+-- local Storage = getgenv().NebulaHub.Storage
+
+local Enums = {
+  ["UnitType"] = {Name = "UnitType", Value = 5};
+}
 
 return function(Content)
   print("ran test.lua, and also:")
