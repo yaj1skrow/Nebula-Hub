e13138569d0215835275326464eacd6eabc002e7
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-03 19:41:34 +0800

    Update Test.lua

--- NebulaHub/Games/Universal/LogicAPI/Test.lua
+++ NebulaHub/Games/Universal/LogicAPI/Test.lua
@@ -1,33 +1,15 @@
--- print(getgenv().NebulaHub)
--- print(getgenv().NebulaHub.Dependencies)
--- print(getgenv().NebulaHub.Loaded["Universal"])
--- print(getgenv().NebulaHub.Loaded["Universal"].GlobalData)
-
--- local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal
--- local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums
+local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal
+local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums
 -- local GlobalData = getgenv().NebulaHub.Loaded["Universal"].GlobalData
 
--- local Player = getgenv().LocalPlayer;
--- local Character = Player.Character or Player.CharacterAdded:Wait();
+local Player = getgenv().LocalPlayer;
+local Character = Player.Character or Player.CharacterAdded:Wait();
   
 -- local Assets = getgenv().NebulaHub.MainUI:WaitForChild("Dependencies"):WaitForChild("Assets");
 -- local Storage = getgenv().NebulaHub.Storage
 
--- local Enums = {
---   ["UnitType"] = {Name = "UnitType", Value = 5};
--- }
-
--- return function(Content)
---   print("ran test.lua, and also:")
---   print(Content)
---   Content:addUnit("Test", Enums.UnitType.Info, {
---       Text = "If you can see this, it means that it worked.";
---       Initialize = function()
---         print("success");
---       end;
---     });
--- end
-
-return function ()
-  print("hi?")
+return function(Content)
+  print("ran test.lua, and also:")
+  print(Content)
+  Content:addUnit("Test", Enums.UnitType.Info, {Text = "If you can see this, it means that it worked.";Initialize = function()print("success");end;});
 end
