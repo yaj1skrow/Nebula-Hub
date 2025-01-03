3b9d056c38bd2c23c44dd5a8cc5bf57c735b653c
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-03 17:27:57 +0800

    Update Test.lua

--- NebulaHub/Games/Universal/LogicAPI/Test.lua
+++ NebulaHub/Games/Universal/LogicAPI/Test.lua
@@ -1,1 +1,18 @@
+local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal
+local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums
+local GlobalData = getgenv().NebulaHub.Loaded["Universal"].GlobalData
 
+local Player = getgenv().LocalPlayer;
+local Character = Player.Character or Player.CharacterAdded:Wait();
+  
+local Assets = getgenv().NebulaHub.MainUI:WaitForChild("Dependencies"):WaitForChild("Assets");
+local Storage = getgenv().NebulaHub.Storage
+
+return function(Content)
+  Content:addUnit("Test", Enums.UnitType.Info, {
+      Text = "If you can see this, it means that it worked.";
+      Initialize = function()
+        print("success");
+      end;
+    };
+end
