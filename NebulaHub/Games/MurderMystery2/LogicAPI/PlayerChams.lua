13a6df10fe9f703a16c69ffa760f0254ffb343f5
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-03 17:11:49 +0800

    Update PlayerChams.lua

--- NebulaHub/Games/MurderMystery2/LogicAPI/PlayerChams.lua
+++ NebulaHub/Games/MurderMystery2/LogicAPI/PlayerChams.lua
@@ -2,6 +2,12 @@ local Signal = getgenv().NebulaHub.Dependencies.Manager.Signal
 local Enums = getgenv().NebulaHub.Dependencies.Manager.Enums
 local GlobalData = getgenv().NebulaHub.Loaded["MM2"].GlobalData
 
+local Player = getgenv().LocalPlayer;
+local Character = Player.Character or Player.CharacterAdded:Wait();
+  
+local Assets = getgenv().NebulaHub.MainUI:WaitForChild("Dependencies"):WaitForChild("Assets");
+local Storage = getgenv().NebulaHub.Storage
+
 return function(Content)
   Content:addUnit("Test", Enums.UnitType.Info, {
       Text = "If you can see this, it means that it worked.";
