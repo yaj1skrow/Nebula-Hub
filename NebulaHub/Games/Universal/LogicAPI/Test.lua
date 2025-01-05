acf162201a9100461e8d87773bebf913ff669cc2
yaj_skrow <alcalajhondaniel6@gmail.com>
2025-01-05 11:07:27 +0800

    Update Test.lua

--- NebulaHub/Games/Universal/LogicAPI/Test.lua
+++ NebulaHub/Games/Universal/LogicAPI/Test.lua
@@ -8,14 +8,14 @@ local Character = Player.Character or Player.CharacterAdded:Wait();
 local Assets = getgenv().NebulaHub.MainUI:WaitForChild("Dependencies"):WaitForChild("Assets");
 local Storage = getgenv().NebulaHub.Storage
 
-local Config = getgenv().NebulaHub.Loaded["Universal"].ConfigData
+-- local Config = getgenv().NebulaHub.Loaded["Universal"].ConfigData
 
 return function(Content)
   print("ran test.lua, and also:")
   print(Content)
   Content:addUnit("Test", Enums.UnitType.Info, {Text = "If you can see this, it means that it worked.";Initialize = function()print("success");end;});
-  Config:addBatch("TestSave", {
-      Name = "Test";
-      Value = 100;
-  })
+  -- Config:addBatch("TestSave", {
+  --     Name = "Test";
+  --     Value = 100;
+  -- })
 end
